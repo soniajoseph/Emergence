82f3644589631be86f8bf511e1f1c19b2a33f867
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-06-17 14:56:49 -0400

    plot weights in tests

--- tests/boltzmann_fmnist.py
+++ tests/boltzmann_fmnist.py
@@ -7,6 +7,7 @@ from tensorflow import keras
 
 from boltzmann.boltzmann_machine import BoltzmannMachine
 from preprocess.preprocess_image import *
+from utils.graphing import visualize
 
 print("Loading fashion MNIST...")
 fashion_mnist = keras.datasets.fashion_mnist
@@ -20,7 +21,7 @@ data = [i.flatten() for i in data]
 
 # Train network
 print("Training network...")
-bm = BoltzmannMachine(n_visible_units=data[0].shape[0], n_hidden_units=500, n_epochs=1000, lr=0.01, batch_size=4)
+bm = BoltzmannMachine(n_visible_units=data[0].shape[0], n_hidden_units=64, n_epochs=500, lr=0.01, batch_size=4)
 bm.train(np.array(data))
 
 # Get noisy data
@@ -31,13 +32,9 @@ print("Running network on noisy data...")
 data_hat = [bm.inference(i) for i in noise_data]
 
 # Plot
-print("Plotting results...")
-total_data = data
-total_data += noise_data
+total_data = [i for i in data]
+total_data += [i for i in noise_data]
 total_data += data_hat
-plt.figure()
-for i in range(1, len(total_data) + 1):
-    plt.subplot(3, 4, i)
-    plt.matshow(total_data[i - 1].reshape(row, col), fignum=False)
-plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
-plt.show()
+visualize(np.array(total_data), n_plots=(3, 4), image_shape=(row, col))
+
+visualize(np.array(bm.T).T, n_plots=(8, 8), image_shape=(row, col))
