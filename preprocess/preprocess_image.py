e22519885669c14029c972bfe00cc6687a046d02
andreanne-lemay <andreanne.lemay@polymtl.ca>
2020-06-17 10:46:47 -0400

    preprocess_image: add option for min value to 0

--- preprocess/preprocess_image.py
+++ preprocess/preprocess_image.py
@@ -1,28 +1,30 @@
 import numpy as np
 
-def normalize_binarize(test_image):
-  """
-  Normalize and binarize test_image.
-  """
-  test_image = test_image / 255
-  mean = np.mean(test_image)
-  idx_over = test_image > mean 
-  idx_under = test_image <= mean 
-  test_image[idx_over] = 1
-  test_image[idx_under] = -1
-  return test_image
 
-def noise_image(test_image, percentage):
-  """
-  Args:
-    test_image (nx1 array): Flattened vector.
-    percentage (float): Fraction to make noisy between [0, 1]
+def normalize_binarize(test_image, min_val=-1):
+    """
+    Normalize and binarize test_image.
+    """
+    test_image = test_image / 255
+    mean = np.mean(test_image)
+    idx_over = test_image > mean
+    idx_under = test_image <= mean
+    test_image[idx_over] = 1
+    test_image[idx_under] = min_val
+    return test_image
 
-  Returns:
-    noise_image (nx1 array): Noisy image.
-  """
-  noise_image = test_image.copy()
-  for i in range(int(len(test_image)*percentage)):
-    a = np.random.choice(len(test_image))
-    noise_image[a] = np.negative(noise_image[a])
-  return noise_image
+
+def noise_image(test_image, percentage, min_val=-1):
+    """
+    Args:
+      test_image (nx1 array): Flattened vector.
+      percentage (float): Fraction to make noisy between [0, 1]
+
+    Returns:
+      noise_image (nx1 array): Noisy image.
+    """
+    noise_image = test_image.copy()
+    for i in range(int(len(test_image) * percentage)):
+        a = np.random.choice(len(test_image))
+        noise_image[a] = np.negative(noise_image[a]) if min_val == -1 else not noise_image[a]
+    return noise_image
